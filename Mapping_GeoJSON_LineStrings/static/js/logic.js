6131d715d2a68f619d969fc1b937c890b134432d
Joe Pedroza <jpedroza50@yahoo.com>
2021-04-10 10:49:15 -0500

    Created variable for styles in logic.js

--- Mapping_GeoJSON_LineStrings/static/js/logic.js
+++ Mapping_GeoJSON_LineStrings/static/js/logic.js
@@ -33,15 +33,19 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let torontoData = "https://raw.githubusercontent.com/JoePedroza/Mapping_Earthquakes/MappingGeoJSON_LineStrings/torontoRoutes.json";
+let torontoData = "https://raw.githubusercontent.com/JoePedroza/Mapping_Earthquakes/Mapping_GeoJSON_LineStrings/torontoRoutes.json";
 
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data,{
-    color:"#fffa1",
-    weight: 2,
+    style: myStyle,
     onEachFeature: function(feature,layer) {
       layer.bindPopup("<h2> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
     }
